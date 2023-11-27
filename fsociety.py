1b74aebd71cdf574a268240802562ae037325a96
Menath <93506278+MB-44@users.noreply.github.com>
2023-11-27 16:58:15 +0530

    create | 11/27

---
+++ fsociety.py
@@ -0,0 +1,8 @@
+def printFullName(first,last):
+  # holy shit this is f/* easy
+  print(f"Hello {first} {last}!")
+
+if __name__ == "__main__":
+  firstName = input()
+  lastName = input()
+  printFullName(firstName,lastName)
